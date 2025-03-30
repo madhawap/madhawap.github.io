45abb12b2a69ccd650cf72c043da6fee4a3beaa4
madhawap <madhawa.818@gmail.com>
2025-03-31 03:00:55 +1100

    Add some quick updates

--- js/script.js
+++ js/script.js
@@ -1,37 +1,31 @@
-// let cursor1 = document.querySelector('.cursor-1');
-// let cursor2 = document.querySelector('.cursor-2');
-//
-// window.onmousemove = (e) =>{
-//     cursor1.style.top = e.pageY + 'px';
-//     cursor1.style.left = e.pageX + 'px';
-//     cursor2.style.top = e.pageY + 'px';
-//     cursor2.style.left = e.pageX + 'px';
-// }
+// Toggle navbar menu
+const menu = document.querySelector('#menu-bars');
+const header = document.querySelector('header');
 
-// document.querySelectorAll('a').forEach(links =>{
-//
-//     links.onmouseenter = () =>{
-//         cursor1.classList.add('active');
-//         cursor2.classList.add('active');
-//     }
-//
-//     links.onmouseleave = () =>{
-//         cursor1.classList.remove('active');
-//         cursor2.classList.remove('active');
-//     }
-//
-// });
-
-let menu = document.querySelector('#menu-bars');
-let header = document.querySelector('header');
-
-menu.onclick = () =>{
+menu?.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
-}
+});
 
-window.onscroll = () =>{
+// Close menu on scroll
+window.addEventListener('scroll', () => {
     menu.classList.remove('fa-times');
     header.classList.remove('active');
+});
+
+// Close banner
+document.querySelector('.banner__close')?.addEventListener('click', function () {
+    this.closest('.banner').style.display = 'none';
+});
+
+// Copy project links with tooltip feedback
+function infunction(boxId, spanId) {
+    navigator.clipboard.writeText(`https://madhawaperera.com/#${boxId}`);
+    const tooltip = document.getElementById(spanId);
+    tooltip.innerHTML = "Link copied";
 }
 
+function outFunc(spanId) {
+    const tooltip = document.getElementById(spanId);
+    tooltip.innerHTML = "Copy the link";
+}
